62761274f44395b3a284394e2265859ea6ac2a3b
quodlibetEns <91636891+quodlibetEns@users.noreply.github.com>
2022-03-24 02:38:54 +0000

    Add accessibility menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// 1. FIRST PICTURE HEIGHT FIX
+
 //first we get the total height of the text in the intro
 function getTotalHeight() {
 
@@ -20,3 +22,65 @@ function getTotalHeight() {
 
 let newHeight = getTotalHeight().toString() + "px";
 document.getElementById('right-img-1').style.maxHeight = newHeight;
+
+// 2. ACCESSIBILITY MENU
+
+const accessibilityPopUp = document.getElementById('accessibility-pop-up');
+const accessibilityButton = document.getElementById('accessibility-button');
+const closeAccessibilityPopUp = document.getElementById('close-accessibility-pop-up');
+
+// Trigger and close pop-up
+
+accessibilityButton.addEventListener('click', function() {
+    accessibilityPopUp.style.display = "block";
+})
+
+closeAccessibilityPopUp.addEventListener('click', function() {
+    accessibilityPopUp.style.display = "none";
+})
+
+// store the default state of the items effected
+const root = document.querySelector(':root');
+var contrast = false;
+var largeFont = false;
+var sansSerif = false;
+
+// Toggle high contrast
+
+document.getElementById('high-contrast').addEventListener('click', function() {
+    if (contrast == false) {
+        root.style.setProperty('--background-color', '#fff');
+        root.style.setProperty('--main-color', '#a13');
+        contrast = true;
+    } else if (contrast == true) {
+        root.style.setProperty('--background-color', 'rgba(255, 245, 245, 1)');
+        root.style.setProperty('--main-color', '#cf2946');
+        contrast = false;
+    }
+})
+
+// Toggle large font size
+
+document.getElementById('font-size').addEventListener('click', function() {
+    if (largeFont == false) {
+        root.style.setProperty('font-size', '32px');
+        document.getElementById('header').style.height = "70px";
+        largeFont = true;
+    } else if (largeFont == true) {
+        root.style.setProperty('font-size', '16px');
+        document.getElementById('header').style.height = "50px";
+        largeFont = false;
+    }
+})
+
+// Toggle sans-serif font
+
+document.getElementById('sans-serif').addEventListener('click', function() {
+    if (sansSerif == false) {
+        root.style.setProperty('font-family', 'Arial, Helvetica, sans-serif');
+        sansSerif = true;
+    } else if (sansSerif == true) {
+        root.style.setProperty('font-family', '"Sorts Mill Goudy", serif');
+        sansSerif = false;
+    }
+})
